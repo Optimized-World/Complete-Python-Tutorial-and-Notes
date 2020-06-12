6109ef563189fa96db9676e0a08c29e7efc9a54a
ExplifiedApps <vivekmishraiit@gmail.com>
2020-06-12 10:16:00 +0530

    Tutorial 13

---
+++ 13. Python Exercise 2 - Faulty Calculator.py
@@ -0,0 +1,7 @@
+# Exercise 2 - Faulty Calculator
+# 45 * 3 = 555, 56+9 = 77, 56/6 = 4
+# Design a calculator which will correctly solve all the problems except
+# the following ones:
+# 45 * 3 = 555, 56+9 = 77, 56/6 = 4
+# Your program should take operator  and the two numbers as input from the user
+# and then return the result
