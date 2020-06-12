d28a7cefd375a019f16bb511df6020c9974916c1
ExplifiedApps <vivekmishraiit@gmail.com>
2020-06-12 09:17:35 +0530

    Tutorial 7

---
+++ 7. Python Lists And List Functions.py
@@ -0,0 +1,30 @@
+grocery = ["Harpic", "vim bar", "deodrant", "Bhindi",
+           "Lollypop", 56]
+# print(grocery[5])
+numbers = [2, 7, 9, 11, 3]
+# numbers.remove(9)
+# numbers.pop()
+# numbers.sort()
+# numbers = []
+# numbers.reverse()
+# numbers.append(1)
+# numbers.append(72)
+# numbers.append(5)
+# numbers.insert(2, 67)
+# print(numbers)
+# 3, 11, 9, 7, 2
+# print(numbers)
+# numbers[1] = 98
+# print(numbers)
+# Mutable - can change
+# Immutable - cannot change
+# tp = (1,)
+# print(tp)
+a= 1
+b = 8
+a, b = b,a
+# temp = a
+# a = b
+# b = temp
+print(a, b)
+
