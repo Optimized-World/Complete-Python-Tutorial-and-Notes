9828c59f596b42aa4724cb20e9896bcb164a6295
ExplifiedApps <vivekmishraiit@gmail.com>
2020-06-12 09:34:29 +0530

    Tutorial 10

---
+++ 10. Sets In Python.py
@@ -0,0 +1,12 @@
+s = set()
+# print(type(s))
+# l = [1, 2, 3, 4]
+# s_from_list = set(l)
+# print(s_from_list)
+# print(type(s_from_list))
+s.add(1)
+s.add(2)
+s.remove(2)
+s1 = {4, 6}
+print(s.isdisjoint(s1))
+
