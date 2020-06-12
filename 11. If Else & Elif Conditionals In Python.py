bef8c0afd88e09cd95ae7765c9e344166d82ebb5
ExplifiedApps <vivekmishraiit@gmail.com>
2020-06-12 10:10:13 +0530

    Tutorial 11

---
+++ 11. If Else & Elif Conditionals In Python.py
@@ -0,0 +1,29 @@
+# var1 = 6
+# var2 = 56
+# var3 = int(input())
+# if var3>var2:
+#     print("Greater")
+# elif var3==var2:
+#     print("Equal")
+# else:
+#     print("Lesser")
+
+# list1 = [5, 7, 3]
+# print(15 not in list1)
+# if 15 not in list1:
+#     print("No its not in the list")
+
+# Quiz
+print("What is your age?")
+age = int(input())
+if age<18:
+    print("You cannot drive")
+
+elif age==18:
+    print("We will think about you")
+
+else:
+    print("You can drive")
+
+
+
