d68b5b21aca56fb7a364e64bdc2c00a11c78df61
ExplifiedApps <vivekmishraiit@gmail.com>
2020-06-12 09:26:49 +0530

    Tutorial 8

---
+++ 8. Dictionary & Its Functions Explained.py
@@ -0,0 +1,17 @@
+# Dictionary is nothing but key value pairs
+d1 = {}
+# print(type(d1))
+d2 = {"Harry":"Burger",
+      "Rohan":"Fish",
+      "SkillF":"Roti",
+      "Shubham":{"B":"maggie", "L":"roti", "D":"Chicken"}}
+# d2["Ankit"] = "Junk Food"
+# d2[420] = "Kebabs"
+# print(d2)
+# del d2[420]
+# print(d2["Shubham"])
+# d3 = d2.copy()
+# del d3["Harry"]
+# d2.update({"Leena":"Toffee"})
+# print(d2.keys())
+# print(d2.items())
