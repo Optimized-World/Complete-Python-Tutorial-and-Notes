0cc2e9524f3d2320170259ecb919d85a58cfc5fe
ExplifiedApps <vivekmishraiit@gmail.com>
2020-06-20 12:28:05 +0530

    Tutorial 15

--- 15. While Loops In Python.py
+++ 15. While Loops In Python.py
@@ -1,5 +1,30 @@
 i = 0
 
-while(i<45):
+# While Condition is true 
+# Inside code of while keep runs
+
+# This will keep printing 0
+# while(i<45):
+#     print(i)
+
+# To stop while loop 
+# update i to break the condition
+while(i<8):
     print(i)
     i = i + 1
+# Output :
+# 0
+# 1
+# 2
+# 3
+# 4
+# 5
+# 6
+# 7
+# 8
+
+# Assuming code inside for and while loop is same
+# Both While and for loop takes almost equal time
+# As both converted into same machine code
+
+# So you can use any thing which is convenient
