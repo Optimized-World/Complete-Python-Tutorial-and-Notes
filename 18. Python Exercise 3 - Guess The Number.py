f89aa3dd4c60918d69d1cf83f78efa773aa208e3
ExplifiedApps <vivekmishraiit@gmail.com>
2020-06-21 10:49:07 +0530

    Tutorial 18 : Exercise 3

---
+++ 18. Python Exercise 3 - Guess The Number.py
@@ -0,0 +1,10 @@
+# Take a number n in program
+# Ask user to guess the number 
+# tell user whether number entered is 
+# greater,less or user guessed the numer correctly
+# no . of guesses allowed to user is 9
+# keep printing no of guesses left
+# if user can guess number in allowed chances 
+# Print Game over You win 
+# Print No. of gusess you took to finish
+# else Game over You lose
