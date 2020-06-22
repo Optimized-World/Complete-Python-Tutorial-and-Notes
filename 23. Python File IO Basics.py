eec82fe6229682bca115e3dfb3396289fc2e80b1
ExplifiedApps <vivekmishraiit@gmail.com>
2020-06-22 15:01:40 +0530

    Tutorial 23

---
+++ 23. Python File IO Basics.py
@@ -0,0 +1,28 @@
+########### Python File IO Basics #########
+
+# Two types of memory
+# 1. Volatile Memory : Data cleared as System Shut Down
+# Example : RAM
+# 2. Non-Volatile Memory : Data remains saved always.
+# Example : Hard Disk
+
+# File : In Non-Volatile Memory we store Data as File
+# File may be text file,binary file(Ex - Image,mp3),etc.
+
+# Different Modes of Opening files in Python
+
+"""
+"r" - Open file for reading
+"w" - Open a file for writing
+"x" - Create file if not exists
+"a" - Add more content to a file/ append at end in file
+"t" - open file in text mode 
+"b" - open file in binary mode
+"+" - for update (read and write both)
+"""
+
+# Question of the tutorial:
+# How to print docstring of func1()
+
+
+# Answer : print(func1.__doc__)
