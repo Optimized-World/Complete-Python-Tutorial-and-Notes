0c4ec220d0fa6a689aed7a0b376773f45e6b221c
ExplifiedApps <vivekmishraiit@gmail.com>
2020-06-22 15:05:42 +0530

    Tutorial 23

--- 23. Python File IO Basics.py
+++ 23. Python File IO Basics.py
@@ -12,11 +12,11 @@
 # Different Modes of Opening files in Python
 
 """
-"r" - Open file for reading
+"r" - Open file for reading - Default mode
 "w" - Open a file for writing
 "x" - Create file if not exists
 "a" - Add more content to a file/ append at end in file
-"t" - open file in text mode 
+"t" - open file in text mode - Default mode
 "b" - open file in binary mode
 "+" - for update (read and write both)
 """
