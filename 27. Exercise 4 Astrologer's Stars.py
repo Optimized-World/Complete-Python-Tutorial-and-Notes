6d3ced9ef581009c834860c16e3742a603f483c8
unknown <vivekmishraiit@gmail.com>
2020-06-29 07:38:18 +0100

    Tutorial 27 : Exercise 4

---
+++ 27. Exercise 4 Astrologer's Stars.py
@@ -0,0 +1,29 @@
+############### Exercise 4 : Astrologer's Stars ########
+
+# Question :
+
+## Pattern Printing ## 
+
+# Input :
+
+# Integer n
+# Boolean = True or False
+
+# Expected Output :
+
+# Given: True n=4
+# Output:
+# *
+# **
+# ***
+# ****
+
+# Given: False n=4
+# Output:
+# ****
+# ***
+# **
+# *
+
+# Solve Here :
+  
